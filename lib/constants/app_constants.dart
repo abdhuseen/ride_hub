1f150a6f697aacdef599d877e7937dbdd9cc5e2e
abdhuseen <72824642+abdhuseen@users.noreply.github.com>
2025-06-21 16:24:43 +0300

    add screen util

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,92 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
 class AppConstants {
 
   static const baseURL="";
+
+  //AppColors
+
+  /// used for button background and may use for text code is #353392
+  static const primaryColor=Color(0xff353392);
+  ///used for text and may use as background code is #FFFFFF
+  static const secondaryColor=Color(0xffffffff);
+  ///star background color 1 code is #F2CF63
+  static const starColor=Color(0xfff2cf63);
+  ///star background color 2 code is #2CF63
+  static const starColor2=Color(0xfff2cf63);
+  ///used for text color code is #0D0D0D
+  static const primaryTextColor=Color(0xff0d0d0d);
+  ///used for text color code is #000000
+  static const primaryTextColor2=Color(0xff000000);
+  ///used for text color code is #16161A
+  static const secondaryTextColor1=Color(0xff16161a);
+  ///used for text color code is #1D1D1D
+  static const secondaryTextColor2=Color(0xff1d1d1d);
+  ///used for text color code is #1E1E1E
+  static const secondaryTextColor3=Color(0xff1e1e1e);
+  ///used for text color code is #757575
+  static const secondaryTextColor4=Color(0xff757575);
+  ///used for text color code is  #B3B3B3
+  static const secondaryTextColor5=Color(0xffb3b3b3);
+  ///used for text color code is  #565656
+  static const secondaryTextColor6=Color(0xff565656);
+  ///used for text color code is  #525252
+  static const secondaryTextColor7=Color(0xff525252);
+  ///used for text color code also hint color is  #666666
+  static const secondaryTextColor8=Color(0xff666666);
+  ///used for text color code is   #303030
+  static const secondaryTextColor9=Color(0xff303030);
+  ///used for text color code is    #F5F5F5
+  static const secondaryTextColor10=Color(0xfff5f5f5);
+  ///used for text color code is    #1A1A1A
+  static const secondaryTextColor11=Color(0xff1a1a1a);
+  ///used for text color code is    #1E1E1E
+  static const secondaryTextColor12=Color(0xff1e1e1e);
+
+  ///used for text hint color code is #464646
+  static const hintTextColor1=Color(0xff464646);
+  ///used for text hint color code is #4D4D4D
+  static const hintTextColor2=Color(0xff4d4d4d);
+  ///used for text hint color code is #858484
+  static const hintTextColor3=Color(0xff858484);
+
+  ///background color code is #1A1A1A
+ static const backgroundColor1=Color(0xff1A1A1A);
+  ///background color code is #2CB67D
+  static const backgroundColor2=Color(0xff2cb67d);
+  ///background color code is #61616129
+  static const backgroundColor3=Color(0x61616129);
+  ///background color code is #EAECF0
+  static const backgroundColor4=Color(0xffeaecf0);
+  ///background color code is #E2E2E2
+  static const backgroundColor5=Color(0xffe2e2e2);
+
+
+  //app font size
+  static double size1=40;
+  static double size2=32;
+  static  double size3=25;
+  static  double size4=24;
+  static  double size5=20;
+  static  double size6=16;
+  static  double size7=15;
+  static  double size8=14;
+  static  double size9=12;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
