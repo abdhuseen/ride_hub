8fa649bb1aaf828a8f8bfdb1df4e0e0e839ecef3
EngMuna <engmuna2002m@gmail.com>
2025-06-25 20:46:06 +0300

    change into

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ class MyApp extends StatelessWidget {
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
           ),
-          locale: Locale('en'),
+
           localizationsDelegates: const [
             AppLocalizations.delegate,
             GlobalMaterialLocalizations.delegate,
