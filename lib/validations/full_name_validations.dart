fa786a184f783d012f4e9023f51c5cbaf42586a9
abdhuseen <72824642+abdhuseen@users.noreply.github.com>
2025-06-25 22:29:23 +0300

    some valid editions

--- lib/validations/full_name_validations.dart
+++ lib/validations/full_name_validations.dart
@@ -2,7 +2,7 @@ class FullNameValidations  {
 
   static bool isFullNameEmpty(String name)=>name.isEmpty;
   static bool isCorrectFullNameFormat(String name) {
-    final RegExp nameRegex = RegExp(r'^[a-zA-Z ]+$');
+    final RegExp nameRegex = RegExp(r'^[a-zA-Z\u0600-\u06FF ]+$');
     return nameRegex.hasMatch(name);
   }
   static bool isValidFullName(String fullName)=>!isFullNameEmpty(fullName)&&isCorrectFullNameFormat(fullName);
